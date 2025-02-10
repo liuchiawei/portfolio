bc1bdfa94be8e56e2d7b5205580ab5461a07af09
LIU CHIAWEI <doublecheap@gmail.com>
2025-02-10 11:26:47 +0900

    basic page layout

---
+++ src/components/BackToTop.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { ArrowUp } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export default function BackToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // height of the Button to show: 852px( iPhone 14 Pro )
+      if (window.scrollY > 852) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    // cleanup function
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <Button
+      className={`${
+        isVisible ? "opacity-100" : "opacity-0"
+      } fixed bottom-8 right-8 cursor-pointer transition-opacity duration-300`}
+      onClick={handleClick}
+    >
+      <ArrowUp className="w-4 h-4" />
+    </Button>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer({ name }: { name: string }) {
+  return (
+    <footer className="text-center bg-linear-to-b from-accent to-rose-600 text-white py-12">
+      © {new Date().getFullYear()} {name} <br /> All rights reserved
+    </footer>
+  );
+}
