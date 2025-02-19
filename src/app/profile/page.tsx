a282114038877caaf0857c0706e32bf07f5d8ce8
LIU CHIAWEI <doublecheap@gmail.com>
2025-02-19 11:35:03 +0900

    update timeline text

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -15,7 +15,7 @@ const data: TimelineEntry[] = [
     year: 2011,
     title: "創業",
     content:
-      "私は2011年に、デジタルアートとイラストレーションの分野でキャリアをスタートさせました。当初は独学で技術を磨き、徐々にクライアントワークを受注するようになりました。デジタルツールとトラディショナルな手法を組み合わせた独自のスタイルを確立し、様々なプロジェクトに携わってきました。特に、キャラクターデザインやコンセプトアート、編集イラストなどの分野で実績を重ねています。また、個人制作では日本の伝統文化やポップカルチャーからインスピレーションを得た作品を制作し、国内外の展示会やアートフェアに参加してきました。デジタルアートの可能性を追求しながら、常に新しい表現方法を模索し続けています。技術の進歩とともに変化するクリエイティブの世界で、革新的なアプローチと確かな技術力を武器に、クライアントの要望に応えながら自身の作風も大切にしています。これまでの経験を活かしながら、より多くの人々に感動を届けられるようなアート作品を生み出していきたいと考えています。",
+      "2011年、インフォグラフィックを主要な創作形式とするデザイン会社「Re:lab」を共同設立しました。コアコンセプトは「理性的なデータを楽しく表現し、感性的な内容を客観的に伝える」ことです。主に情報伝達のコンサルティングやデザイン制作を手掛け、設計、戦略、プロモーションといった包括的なサービスを提供しています。",
     emoji: "💼",
   },
 
@@ -23,35 +23,33 @@ const data: TimelineEntry[] = [
     year: 2012,
     title: "卒業",
     content:
-      "卒業Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+      "台湾大学経営学部を卒業し、卒業後も引き続き起業活動に取り組んでいます。",
     emoji: "🎓",
   },
   {
     year: 2017,
     title: "独立",
     content:
-      "独立Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+      "フリーランスのイラストレーターおよびグラフィックデザイナーとして活動。デジタルアートの可能性を追求しながら、常に新しい表現方法を模索し続けています。",
     emoji: "🧑‍🎨",
   },
   {
     year: 2022,
     title: "來日",
-    content:
-      "來日Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+    content: "2022年、コロナ禍が終わった後、東京に移住。",
     emoji: "🗼",
   },
   {
     year: 2023,
     title: "プロボクシング",
-    content:
-      "WEBデザインに転向。Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+    content: "2023年、プロボクサーとしてデビュー。",
     emoji: "🥊",
   },
   {
     year: 2024,
     title: "WEBデザイン",
     content:
-      "WEBデザインに転向。Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+      "新しい創作の可能性を探求し、より多くの技術を学ぶために、HAL東京のWEB学科に入学。",
     emoji: "💻",
   },
 ];
