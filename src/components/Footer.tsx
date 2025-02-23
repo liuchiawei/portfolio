578cfb86d8460b601a82144128c2ce29befe2e77
LIU CHIAWEI <doublecheap@gmail.com>
2025-02-23 13:20:58 +0900

    add SNS links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,9 @@
+import SNSLinks from "./SNSLinks";
+
 export default function Footer({ name }: { name: string }) {
   return (
     <footer className="text-center p-20 bg-linear-to-b from-transparent to-foreground/10">
+      <SNSLinks />
       <p className="text-center text-sm text-foreground/50">
         © {new Date().getFullYear()} {name}　|　All rights reserved
       </p>
